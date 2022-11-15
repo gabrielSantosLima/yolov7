0594de527641f995f90ba3f634cc67c0730b58fb
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2022-11-15 14:52:49 -0300

    DrawPosition Changes

--- app/moves/moves.py
+++ app/moves/moves.py
@@ -5,9 +5,13 @@ import numpy as np
 #    x1, y1    x2, y2     x3,  y3    x4,  y4
 # [[50, 50], [250, 50], [250, 200], [50, 200]]
 
-def draw_moves(frame, contours_list):
-    img = frame.copy()
-    for contours in contours_list:
+def draw_positions(image, chessObject_list):
+    img = image.copy()
+    for chessObject in chessObject_list:
+        contours = [chessObject.top_left, 
+                    chessObject.top_right,
+                    chessObject.bottom_right,
+                    chessObject.bottom_left]
         points = np.array([contours])
         cv2.drawContours(img, points, 0, (128, 255, 0), 3)
     return img
