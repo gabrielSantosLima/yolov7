5df2623491cb307c65fd3c48f6a7d3a41b88c1fc
FMABr <felipemuamorim@gmail.com>
2022-11-15 16:22:15 -0400

    Board detection

---
+++ app/board/board.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+from app.chesspiece import Square
+
+SHARPEN_MATRIX = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
+
+def adjust_size(image, new_width):
+    height, width = image.shape[:2]
+    ratio = float(new_width) / width
+    
+    new_height = int(height * ratio)
+    
+    image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+    return image, ratio
+
+def translate_contours(contours, ratio):
+    return [np.array([[(np.array(point[0])*ratio).astype(int)] for point in contour]) for contour in contours]
+
+def is_approx_square(contour, percent):
+    bounds = cv2.minAreaRect(contour)
+    width, height = bounds[1]
+    
+    return width*(1-percent) <= height <= width*(1+percent)
+
+def square_contours(contours, percent = 0.25):
+    return [np.int0(cv2.boxPoints(cv2.minAreaRect(contour))) for contour in contours if is_approx_square(contour, percent)]
+
+def find_contours(image, area = None, morph = False, sharpen = False):
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+
+    if sharpen:
+        img_blurred = cv2.medianBlur(img_gray, 5)
+        img_sharpened = cv2.filter2D(img_blurred, -1, SHARPEN_MATRIX)
+        img_gray = img_sharpened
+
+    img_bin = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 20)
+        
+    if morph:
+        img_bin = cv2.erode(img_bin, np.ones((3,3), np.uint8), iterations=1)
+
+    contours = cv2.findContours(img_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    if area is not None:
+        contours = list(filter(lambda contour: area[0] < cv2.contourArea(contour) < area[1], contours))
+
+    return contours
+
+
+def create_board(squares: list[Square]):
+    qtd = len(squares)
+    assert qtd == 64, f'Não é possivel montar o tabuleiro com {qtd} posições.'
+
+    squares.sort(key=lambda square: square.top_left[1])
+
+    board = [squares[i:i+8] for i in range(0, 64, 8)]
+
+    for row in board:
+        row.sort(key=lambda square: square.top_left[0])
+    
+    return board
+
+def detect_board(image, area = (200, 10000)):
+    width = image.shape[1]
+    ratio = None
+    if width > 1200:
+        image, ratio = adjust_size(image, 800)
+    
+    contours = find_contours(image, area, sharpen = True, morph = True)
+    
+    if ratio is not None:
+        contours = translate_contours(contours, (1/ratio))
+    
+    contours = square_contours(contours)
+
+    squares = []
+    for contour in contours:
+        bottom_left, top_left, top_right, bottom_right = contour
+        squares.append(Square(top_left, top_right, bottom_right, bottom_left))
+
+    print(f'Foram detectados {len(squares)} quadrados na imagem.')
+
+    board = create_board(squares)
+
+    return board
